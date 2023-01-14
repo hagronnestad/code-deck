7e8e44c4248afd070e509fc485a6c430ce198e18
Hein Andre Grønnestad <hag@haugnett.no>
2023-01-14 03:10:03 +0100

    Implemented PerformanceCounters plugin for CPU and RAM usage

---
+++ CodeDeck.Plugins/Plugins/PerformanceCounters/PerformanceCounters.cs
@@ -0,0 +1,78 @@
+﻿using CodeDeck.PluginAbstractions;
+using System.Diagnostics;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+
+namespace CodeDeck.Plugins.Plugins.Counter;
+
+[SupportedOSPlatform("windows")]
+public class PerformanceCounters : CodeDeckPlugin
+{
+    public class CpuUsageTile : Tile
+    {
+        private bool _deInit = false;
+
+        private PerformanceCounter _cpuUsageCounter = new("Processor Information", "% Processor Utility", "_Total");
+
+        public override async Task Init()
+        {
+            _ = Task.Run(UpdateTile);
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateTile()
+        {
+            while (!_deInit)
+            {
+                var cpuUsage = (int)_cpuUsageCounter.NextValue();
+                Text = $"CPU\n{cpuUsage} %";
+                await Task.Delay(1000);
+            }
+        }
+
+        public override Task DeInit()
+        {
+            _deInit = true;
+            return base.DeInit();
+        }
+
+        public override async Task OnTilePressDown()
+        {
+            await Task.CompletedTask;
+        }
+    }
+
+    public class MemoryUsageTile : Tile
+    {
+        private bool _deInit = false;
+
+        private PerformanceCounter _memUsageCounter = new("Memory", "% Committed Bytes In Use");
+
+        public override async Task Init()
+        {
+            _ = Task.Run(UpdateTile);
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateTile()
+        {
+            while (!_deInit)
+            {
+                var memUsage = (int)_memUsageCounter.NextValue();
+                Text = $"RAM\n{memUsage} %";
+                await Task.Delay(1000);
+            }
+        }
+
+        public override Task DeInit()
+        {
+            _deInit = true;
+            return base.DeInit();
+        }
+
+        public override async Task OnTilePressDown()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
