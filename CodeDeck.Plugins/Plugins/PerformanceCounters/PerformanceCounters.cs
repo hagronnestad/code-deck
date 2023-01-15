7321e4b34fcc92b42c3ca2bee3826283482c8600
Hein Andre Grønnestad <hag@haugnett.no>
2023-01-15 14:15:48 +0100

    Added GpuUsageTile to PerformanceCounters plugin

--- CodeDeck.Plugins/Plugins/PerformanceCounters/PerformanceCounters.cs
+++ CodeDeck.Plugins/Plugins/PerformanceCounters/PerformanceCounters.cs
@@ -1,4 +1,5 @@
 ﻿using CodeDeck.PluginAbstractions;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.Versioning;
 using System.Threading.Tasks;
@@ -73,6 +74,86 @@ public class PerformanceCounters : CodeDeckPlugin
         public override async Task OnTilePressDown()
         {
             await Task.CompletedTask;
+        }
+    }
+
+    public class GpuUsageTile : Tile
+    {
+        private bool _deInit = false;
+
+        public override async Task Init()
+        {
+            _ = Task.Run(UpdateTile);
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateTile()
+        {
+            while (!_deInit)
+            {
+                var usage = (int) await GetGpuUsage();
+                Text = $"GPU\n{usage} %";
+                await Task.Delay(1000);
+            }
+        }
+
+        public override Task DeInit()
+        {
+            _deInit = true;
+            return base.DeInit();
+        }
+
+        public override async Task OnTilePressDown()
+        {
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Taken from:
+        /// https://github.com/rocksdanister/lively/blob/d4972447531a0a670ad8f8c4724c7faf7c619d8b/src/livelywpf/livelywpf/Helpers/HWUsageMonitor.cs#L143
+        /// </summary>
+        /// <returns></returns>
+        private async Task<float> GetGpuUsage()
+        {
+            try
+            {
+                var category = new PerformanceCounterCategory("GPU Engine");
+                var counterNames = category.GetInstanceNames();
+                var gpuCounters = new List<PerformanceCounter>();
+                var result = 0f;
+
+                foreach (string counterName in counterNames)
+                {
+                    if (counterName.EndsWith("engtype_3D"))
+                    {
+                        foreach (PerformanceCounter counter in category.GetCounters(counterName))
+                        {
+                            if (counter.CounterName == "Utilization Percentage")
+                            {
+                                gpuCounters.Add(counter);
+                            }
+                        }
+                    }
+                }
+
+                gpuCounters.ForEach(x =>
+                {
+                    _ = x.NextValue();
+                });
+
+                await Task.Delay(1000);
+
+                gpuCounters.ForEach(x =>
+                {
+                    result += x.NextValue();
+                });
+
+                return result;
+            }
+            catch
+            {
+                return 0f;
+            }
         }
     }
 }
