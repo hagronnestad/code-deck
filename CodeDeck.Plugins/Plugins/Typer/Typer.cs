d8b3e16e33a970d42cf48111cd414db05fcd5e0b
Hein Andre Grønnestad <hag@haugnett.no>
2023-01-21 16:06:42 +0100

    Added Typer-plugin

---
+++ CodeDeck.Plugins/Plugins/Typer/Typer.cs
@@ -0,0 +1,29 @@
+﻿using CodeDeck.PluginAbstractions;
+using InputSimulatorEx;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeDeck.Plugins.Plugins.Counter;
+
+public class Typer : CodeDeckPlugin
+{
+    public class TyperTile : Tile
+    {
+        private string? _text;
+        private InputSimulator _inputSimulator = new();
+
+        public override Task Init(CancellationToken cancellationToken)
+        {
+            Settings?.TryGetValue("text", out _text);
+
+            return base.Init(cancellationToken);
+        }
+
+        public override async Task OnTilePressDown(CancellationToken cancellationToken)
+        {
+            _inputSimulator.Keyboard.TextEntry(_text ?? "No text specified!");
+
+            await Task.CompletedTask;
+        }
+    }
+}
