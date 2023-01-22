9508606af25acb6da41b64590f29d458b29f83b6
Hein Andre Grønnestad <hag@haugnett.no>
2023-01-22 13:53:54 +0100

    Use SettingAttribute, added UseShellExecute property, removed ShellRunTile

--- CodeDeck.Plugins/Plugins/Runner/Runner.cs
+++ CodeDeck.Plugins/Plugins/Runner/Runner.cs
@@ -10,58 +10,50 @@ namespace CodeDeck.Plugins.Plugins.Runner
 {
     public class Runner : CodeDeckPlugin
     {
-        private static HttpClient _httpClient = new();
+        private static readonly HttpClient _httpClient = new();
 
         public class RunTile : Tile
         {
+            [Setting] public string? Program { get; set; }
+            [Setting] public string? Arguments { get; set; }
+            [Setting] public bool? UseShellExecute { get; set; }
+
             public override Task OnTilePressDown(CancellationToken cancellationToken)
             {
-                var program = Settings?["program"];
-                if (program == null) return Task.CompletedTask; ;
-
-                Process.Start(program);
+                if (Program is not null)
+                {
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = Program,
+                        Arguments = Arguments ?? "",
+                        UseShellExecute = UseShellExecute ?? false
+                    });
+                }
 
                 return Task.CompletedTask;
             }
         }
 
-        public class ShellRunTile : Tile
-        {
-            public override async Task OnTilePressDown(CancellationToken cancellationToken)
-            {
-                var program = Settings?["program"];
-                if (program == null) return;
-
-                Process.Start(new ProcessStartInfo()
-                {
-                    FileName = program,
-                    UseShellExecute = true,
-                });
-
-                await Task.CompletedTask;
-            }
-        }
-
         public class OpenWebsiteTile : Tile
         {
+            [Setting] public string? Url { get; set; }
+
             public override async Task Init(CancellationToken cancellationToken)
             {
-                var url = Settings?["url"];
-                if (url == null) return;
+                if (Url == null) return;
 
-                var uri = new Uri(url);
+                var uri = new Uri(Url);
                 var favicon = await _httpClient.GetByteArrayAsync($"http://www.google.com/s2/favicons?domain={uri.Host}&sz=64");
                 Image = Image.Load(favicon);
             }
 
             public override async Task OnTilePressDown(CancellationToken cancellationToken)
             {
-                var url = Settings?["url"];
-                if (url == null) return;
+                if (Url == null) return;
 
                 Process.Start(new ProcessStartInfo()
                 {
-                    FileName = url,
+                    FileName = Url,
                     UseShellExecute = true,
                 });
 
