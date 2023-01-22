a13a2f9a3556204ebea0d645c87b220b211f2692
Hein Andre Grønnestad <hag@haugnett.no>
2023-01-22 20:41:43 +0100

    Added CodeDeck.Linux wrapper project

---
+++ CodeDeck.Linux/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace CodeDeck.Linux
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CodeDeck.Program.Main(args);
+        }
+    }
+}
